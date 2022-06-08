758d7cdd31f17fcff4aa322ff65e90ba224020eb
Obasi <objtorti@hotmail.com>
2022-06-08 03:35:02 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
-#include <unistd.h>
-#include "main.h"                                                                                                                               
+#include <stdio.h>
+ #include "main.h"                                                                                                                               
                                                                                                                                                 
 /**                                                                                                                                             
- * main - Check description                                                                                                                     
- * Description: It prints the word _putchar, followed by a new line.                                                                            
- * Return: 0.                                                                                                                                   
+ * main = Entry point                                                                                                                           
+ *                                                                                                                                              
+ * Return: Always 0 (Success)                                                                                                                   
  */                                                                                                                                             
 int main(void)                                                                                                                                  
 {                                                                                                                                               
-        char word[8] = "_putchar";                                                                                                              
-        int i;                                                                                                                                  
-                                                                                                                                                
-        for (i = 0; i < 8; i++)                                                                                                                 
-                _putchar(word[i]);                                                                                                              
-        _putchar('\n');                                                                                                                         
-                                                                                                                                                
+        printf("_putchar\n");                                                                                                                   
         return (0);                                                                                                                             
-}
+} 
