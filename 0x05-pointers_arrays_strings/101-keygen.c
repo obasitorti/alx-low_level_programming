b11508d57c29671483f4c207a081aae191530d1d
Obasi <objtorti@hotmail.com>
2022-06-13 17:55:38 -0700

    Corrections

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,61 +1,47 @@
 #include <stdio.h>
 
+#include <stdlib.h>
+
+#include <time.h>
+
 /**
- * function to generate password
+
+ *main - generates random valid passwords
+
+ *Return: 0 (on success).
+
+ *
+
  */
-void generatepw(int N)
+
+int main(void)
+
 {
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
-    char numbers[] = "0123456789";
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
+
+	int pass, sum;
+
+
+
+	srand(time(NULL));
+
+	sum = 0;
+
+	while (sum <= 2645)
+
+	{
+
+		pass = (rand() % 128);
+
+		sum += pass;
+
+		printf("%c", pass);
+
+	}
+
+	printf("%c", 2772 - sum);
+
+
+
+	return (0);
+
 }
