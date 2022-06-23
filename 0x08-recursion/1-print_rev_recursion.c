def3e843c21b5b15d2385818d545b2e374039abe
obasitorti <104761686+obasitorti@users.noreply.github.com>
2022-06-23 02:06:37 +0100

    correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* _print_rev_recursion - Prints a string in reverse followed by a new line
+*
+* @s: Pointer to use
+*
+* Return: void
+*/
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
