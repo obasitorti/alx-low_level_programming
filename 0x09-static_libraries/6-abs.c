2a98407defbbbab3b28f626875b88147c90f43d7
obasitorti <104761686+obasitorti@users.noreply.github.com>
2022-06-25 04:23:08 +0100

    update

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Check main
+ * r: An integer
+ * Description: function that computes the absolute value of an integer
+ * Return: Absolute value of r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
